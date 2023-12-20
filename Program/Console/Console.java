c40a03ac27d1fec2fd8e685a2c72a2125e0cb671
Evgeniy <lukik884@gamil.com>
2023-12-20 23:23:04 +0300

    консоле

---
+++ Program/Console/Console.java
@@ -0,0 +1,18 @@
+public class Console{
+    private Scanner scanner;
+    private String info;
+
+    public Console(){
+        scanner = new  Scanner(System.in);
+    }
+
+    private void work(){
+        System.out.println("Введите разделяя пробелом: Фамилию Имя Отчество датурождения номертелефона пол(f\\m)\n");
+        this.info = scanner.nextLine();
+    }
+
+    public String getInfo(){
+        work();
+        return info;
+    }
+}
---
+++ Program/main.java
@@ -0,0 +1,7 @@
+ public class Main {
+    public static void main (String[] args) {
+        Console console = new Console();
+        String info = console.getInfo();
+        System.out.println(info);
+    }
+}
